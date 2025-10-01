96e0938dd195bc54f5525ebd4b851bb74772e922
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2025-10-01 17:10:42 -0300

    Reogarnizacao dos arquivos

--- Projetos/01_basico/app_imc/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-// import 'package:myapp/home_page.dart';
-import 'package:app_imc/pages/login_page.dart';
-
-void main() {
-  runApp(MaterialApp(home: LoginPage()));
-}
---
+++ Projetos/sala_de_aula/cloneflix/android/app/src/main/kotlin/com/example/cloneflix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cloneflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
